b06fd3125c60359f8bb1ee607913e11f411109ce
LUCAS ARAUJO EMMERICH <18969750703@cefet-rj.br>
2020-11-21 19:30:56 -0300

    Adiciona muda de config

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const config = require('../package.json').config;
 
 const api = axios.create({
-    baseURL : config.dsvApiAddress
+    baseURL : config.apiAddress
 });
 
 export default api;
